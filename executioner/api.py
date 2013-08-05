70d8d1f2820b7c1a987f72bb9f351c4d888b8f73
JuhaS <suomalainen.juha@gmail.com>
2013-08-05 10:23:06 +0800

    Minor bigfix on errback.

--- executioner/api.py
+++ executioner/api.py
@@ -121,7 +121,7 @@ class RunCommandHandler(Resource):
         return
 
     def _errback(self, data, request=None):
-        request.write(jsonify(error_page(request)))
+        request.write(jsonify(request, error_page(request)))
         request.finish()
         return
 
